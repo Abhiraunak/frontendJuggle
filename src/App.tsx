e2e62ffe26488466c0df732e5fe7772dc501d981
Abhishek <abhishekkp1721@hotmail.com>
2024-11-24 15:51:06 +0530

    Structure the file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+// import './App.css'
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from './Pages/signin';
+import Signup from './Pages/signup';
 
 function App() {
   
 
   return (
     <>
-      <div>Hi there, after very long time</div>
-      good to see you back 😍
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<Signin />}/>
+        <Route path="/signup" element={<Signup />}/>
+      </Routes>
+    </BrowserRouter>
+
+      
     </>
   )
 }

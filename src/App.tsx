7a4808b4fa555bd01242aef23227d040720dd1bd
Abhishek <abhishekkp1721@hotmail.com>
2024-11-24 15:06:41 +0530

    improving the frontend skill and reusability of component

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>Hi there, after very long time</div>
+      good to see you back 😍
+    </>
+  )
+}
+
+export default App
